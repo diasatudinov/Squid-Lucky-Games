057cedd56686199a4c6adf4e95a03cfcb113c6f3
Dias <dias.atudinov@gmail.com>
2025-03-24 17:25:36 +0500

    LoadingView

--- Squid Lucky Games/Squid Lucky Games/SLModules/SLMenu/LoadingViewSL.swift
+++ Squid Lucky Games/Squid Lucky Games/SLModules/SLMenu/LoadingViewSL.swift
@@ -8,10 +8,77 @@
 import SwiftUI
 
 struct LoadingViewSL: View {
+    @State private var progress: Double = 0.0
+    @State private var timer: Timer?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                
+                VStack {
+                    Spacer()
+                    ZStack {
+                        VStack(spacing: 10) {
+                            
+                            Image(.logoIconSL)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SLDeviceInfo.shared.deviceType == .pad ? 400:200)
+                            
+                            Image(.loadingText)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SLDeviceInfo.shared.deviceType == .pad ? 40:20)
+                            HStack {
+                                Spacer()
+                                ZStack {
+                                    Image(.loaderBg)
+                                        .resizable()
+                                        .scaledToFit()
+                                    
+                                    Image(.loaderFront)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .mask(alignment: .leading) {
+                                            Rectangle()
+                                                .frame(width: (geometry.size.width * progress))
+                                                .animation(.easeInOut, value: progress)
+                                        }
+                                        .frame(height: SLDeviceInfo.shared.deviceType == .pad ? 39.6:19.8)
+                                    
+                                }.frame(height: SLDeviceInfo.shared.deviceType == .pad ? 67:33.5)
+                                Spacer()
+                            }
+                        }
+                    }
+                    .foregroundColor(.black)
+                    Spacer()
+                }
+            }.background(
+                Image(.loadingViewBgSL)
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+            )
+            .onAppear {
+                startTimer()
+            }
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 1 {
+                progress += SLDeviceInfo.shared.deviceType == .pad ? 0.005:0.003
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
+
 
 #Preview {
     LoadingViewSL()
