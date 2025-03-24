e256eaeda4b21ff114a0ccf3332c580560260207
Dias <dias.atudinov@gmail.com>
2025-03-24 16:23:22 +0500

    first commit

---
+++ Squid Lucky Games/Squid Lucky Games/Squid_Lucky_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Squid_Lucky_GamesApp.swift
+//  Squid Lucky Games
+//
+//  Created by Dias Atudinov on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Squid_Lucky_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
