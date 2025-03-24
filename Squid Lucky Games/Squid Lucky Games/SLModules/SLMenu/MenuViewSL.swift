912dfa3f3ea6ae1940d91f35a6d8058093758190
Dias <dias.atudinov@gmail.com>
2025-03-24 17:26:16 +0500

    Menu View

--- Squid Lucky Games/Squid Lucky Games/SLModules/SLMenu/MenuViewSL.swift
+++ Squid Lucky Games/Squid Lucky Games/SLModules/SLMenu/MenuViewSL.swift
@@ -8,10 +8,117 @@
 import SwiftUI
 
 struct MenuViewSL: View {
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showAchivement = false
+    @State private var showSettings = false
+    
+//    @StateObject var shopVM = ShopViewModelIE()
+//    @StateObject var settingsVM = SettingsViewModelIE()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader { geometry in
+            ZStack {
+                VStack(spacing: 0) {
+                    HStack {
+                        
+                        Image(.experienceBgSL)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: SLDeviceInfo.shared.deviceType == .pad ? 180:90)
+                        
+                        Spacer()
+                        
+                        Button {
+                            showSettings = true
+                        } label: {
+                            Image(.settingsIconSL)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SLDeviceInfo.shared.deviceType == .pad ? 160:80)
+                        }
+                        
+                    }
+                    Spacer()
+                    
+                    HStack {
+                        Button {
+                            showGame = true
+                        } label: {
+                            Image(.playIconSL)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SLDeviceInfo.shared.deviceType == .pad ? 200:100)
+                        }
+                        
+                    }
+                    
+                    Spacer()
+                    HStack {
+                        Button {
+                            showAchivement = true
+                        } label: {
+                            Image(.achivmentIconSL)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SLDeviceInfo.shared.deviceType == .pad ? 140:70)
+                        }
+                        
+                        Spacer()
+                        Button {
+                            showShop = true
+                        } label: {
+                            Image(.shopIconSL)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SLDeviceInfo.shared.deviceType == .pad ? 180:90)
+                        }
+                        
+                    }
+                }.padding()
+            }
+            .background(
+                ZStack {
+                    Image(.menuViewBgSL)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .scaledToFill()
+                }
+                
+            )
+//            .onAppear {
+//                if settingsVM.musicEnabled {
+//                    MusicManagerIE.shared.playBackgroundMusic()
+//                }
+//            }
+//            .onChange(of: settingsVM.musicEnabled) { enabled in
+//                if enabled {
+//                    MusicManagerIE.shared.playBackgroundMusic()
+//                } else {
+//                    MusicManagerIE.shared.stopBackgroundMusic()
+//                }
+//            }
+            .fullScreenCover(isPresented: $showGame) {
+//                ChooseLevelIE(shopVM: shopVM)
+            }
+            .fullScreenCover(isPresented: $showShop) {
+//                ShopViewIE(viewModel: shopVM)
+            }
+            .fullScreenCover(isPresented: $showAchivement) {
+//                ShopViewIE(viewModel: shopVM)
+            }
+            .fullScreenCover(isPresented: $showSettings) {
+//                SettingsView(settings: settingsVM)
+            }
+            
+        }
+        
+        
     }
+    
 }
+
 
 #Preview {
     MenuViewSL()
