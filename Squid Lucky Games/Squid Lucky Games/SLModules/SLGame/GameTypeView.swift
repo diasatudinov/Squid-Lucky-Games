be43be0a5591e7ed0e05c5b2ef15459cee8e49c7
Dias <dias.atudinov@gmail.com>
2025-03-26 21:18:33 +0500

    new files

---
+++ Squid Lucky Games/Squid Lucky Games/SLModules/SLGame/GameTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  GameTypeView.swift
+//  Squid Lucky Games
+//
+//  Created by Dias Atudinov on 26.03.2025.
+//
+
+import SwiftUI
+
+struct GameTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameTypeView()
+}
